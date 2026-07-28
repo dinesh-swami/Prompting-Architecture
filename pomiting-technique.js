35043ee452892c2d86ff52177c209b00560e9465
dinesh-swami <dinesh907989@gmail.com>
2026-07-28 20:45:00 +0530

    inhance the more optimaized vversion to achived things

--- pomiting-technique.js
+++ pomiting-technique.js
@@ -1,14 +1,1 @@
 just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing 
-just testing https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/https://github.com/dinesh-swami/
-just testing 
